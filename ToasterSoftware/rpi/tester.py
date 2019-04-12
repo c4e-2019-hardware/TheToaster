069f27b2fe7398cd895ea0b7116fcde2c00a4d02
ZpengungamerZ <chi.lovvorn@gmail.com>
2019-04-12 02:17:46 +0000

    what is your name

--- ToasterSoftware/rpi/tester.py
+++ ToasterSoftware/rpi/tester.py
@@ -6,8 +6,8 @@ def main():
     ### I am a link to the Utilities Class in the rpiToaster.py file
     rpT = rpiToaster.Utilities()
 
-    print str(rpT.checkHardware())
-
+    print str("rpT.checkHardware"())
+    print str("charge!!!!!!!")
     print str("chi rules")
     print str("penguins are awsome")
     print str("what is your name?")
