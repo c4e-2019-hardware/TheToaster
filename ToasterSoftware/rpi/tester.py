417b99b377ad17677ce64b04d3219d6bce58a3a2
denver-cfman <denver.cfman@gmail.com>
2019-04-16 20:30:05 -0600

    Good

--- ToasterSoftware/rpi/tester.py
+++ ToasterSoftware/rpi/tester.py
@@ -7,18 +7,6 @@ import rpiSoftware
 def main():
     ### I am a link to the Utilities Class in the rpiToaster.py file
     rpT = rpiToaster.Utilities()
-<<<<<<< HEAD
-<<<<<<< HEAD
-    print str("huvbycvdrszawsdfg")
-=======
-
->>>>>>> 8723f8ea8576c9539a516c6052f54165d202d10e
-    print str("rpT.checkHardware"()
-    print str("charge!!!!!!!")
-    print str("chi rules")
-    print str("penguins are awsome")
-    print str("what is your name?")
-=======
     rpS = rpiSoftware.SoftBread()
 
     print str(rpT.checkHardware())
@@ -32,7 +20,5 @@ def main():
     print str("Victory Royale")
 
     return str("Main Function")
-
->>>>>>> master
 if __name__ == "__main__":
     main()
