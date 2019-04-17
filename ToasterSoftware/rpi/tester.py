915a2dbbdfd373baff8d57c5b08ca1c00a209872
Mr Z <denver.cfman@gmail.com>
2019-04-17 01:57:50 +0000

    Added software Class

--- ToasterSoftware/rpi/tester.py
+++ ToasterSoftware/rpi/tester.py
@@ -1,12 +1,21 @@
 import sys
 sys.path.append('../../ToasterHardware')
+sys.path.append('../../ToasterSoftware')
 import rpiToaster
+import rpiSoftware
 
 def main():
     ### I am a link to the Utilities Class in the rpiToaster.py file
     rpT = rpiToaster.Utilities()
+    rpS = rpiSoftware.SoftBread()
 
     print str(rpT.checkHardware())
+
+
+    if rpT.triggerPullDown(side="Left"):
+        rpS.toastNotify(inEmail='zach@thegiezens.com')
+    else:
+        str(error)
 
     print str("Victory Royale")
 
