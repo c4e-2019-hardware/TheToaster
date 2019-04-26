4889b823790c5c9f825b89c213990ede2688deec
MrZ <denver.cfman@gmail.com>
2019-04-26 04:04:49 +0100

    changed to BCM

--- ToasterSoftware/rpi/foo.py
+++ ToasterSoftware/rpi/foo.py
@@ -1,9 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 
-servoPIN = 36
+servoPIN = 16
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
