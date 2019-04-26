dda983fbec8bf19276a9bd8e890a30eddf35648c
cherryblast2002 <48840446+cherryblast2002@users.noreply.github.com>
2019-04-26 13:10:58 -0600

    Update rpiToaster.py

--- ToasterHardware/rpiToaster.py
+++ ToasterHardware/rpiToaster.py
@@ -19,6 +19,29 @@ class Utilities:
 
     def triggerPullDown(self, side=None):
         """ Stuff goes here to trigger the pull down servo to pull the toat down, which side right? left? who knows """
+        import RPi.GPIO as GPIO
+import time
+
+servoPIN = 16
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p.start(0) # Initialization
+try:
+    dlist = [1,2,2.5,5,7.5,10,12.5,13,13.5,13,12.5,10,7.5,5,2.5,2,1]
+    for i in dlist:
+        p.ChangeDutyCycle(i)
+        time.sleep(0.08)
+
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
+
+p.stop()
+GPIO.cleanup()
+        
         return None
 
     def isToastDone(self, side=None):
